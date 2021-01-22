f846cff1c214e8f5517534cdb45c78ec86c62552
Sandy <michaelsandy@gmx.de>
2021-01-22 20:37:20 +0100

    Change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './rage.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          RAAAGEEE!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
